f9980efe13e12c984e7dffa2dfaabd32d7feb8d8
chimpy7 <aleksandar199@hotmail.com>
2026-05-26 10:40:16 +0200

    fixing deploy

--- hemsida/vite.config.ts
+++ hemsida/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hemsida/",
+  base: "/minhemsida/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
